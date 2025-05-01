c37afc7f8a69478e080d00b6dd3b640e34fc4665
Julia Fortt <juliamfortt@gmail.com>
2025-04-30 20:17:19 -0400

    add scripts folder and reorganize project files

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,7 +7,7 @@ var level_records = {}
 func start_level(level_name: String):
 	print("level start: " + level_name)
 	if level_name == "level_1":
-		var level_data_script = preload("res://scenes/level1/level_1_data.gd")
+		var level_data_script = preload("res://scripts/level_1_data.gd")
 		current_level_data = level_data_script.new()
 	elif level_name == "level_2":
 		pass
