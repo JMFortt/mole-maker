fbc84ff4c61023b34c1001a1550e9efbf957ed9f
Julia Fortt <juliamfortt@gmail.com>
2025-05-06 09:30:18 -0400

    fix internal/external timer consistency bug

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-var elapsed_time := 0
-
 func _ready():
 	$UpdateTimer.start()
 
 func _on_update_timer_timeout():
-	elapsed_time += 1
-	var minutes = elapsed_time / 60
-	var seconds = elapsed_time % 60
+	var time_elapsed: int = floor((Time.get_ticks_msec() - GameManager.current_level_data.level_start_time) / 1000.0)
+	var minutes = time_elapsed / 60
+	var seconds = time_elapsed % 60
 	$TimerLabel.text = "%d:%02d" % [minutes, seconds]
