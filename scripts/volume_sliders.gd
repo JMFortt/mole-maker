b4908db651f69f59c20c214a3c34ccc5ae0bdffc
Julia Fortt <juliamfortt@gmail.com>
2025-05-07 18:33:10 -0400

    fix volume slider stuck at max bug

--- scripts/volume_sliders.gd
+++ scripts/volume_sliders.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	$MusicHSlider.value = linear_to_db(AudioManager.music_player.volume_db)
+	$MusicHSlider.value = db_to_linear(AudioManager.music_player.volume_db)
 	$MusicHSlider.connect("value_changed", AudioManager.set_music_volume)
-	$SFXHSlider2.value = linear_to_db(AudioManager.click_player.volume_db)
+	$SFXHSlider2.value = db_to_linear(AudioManager.click_player.volume_db)
 	$SFXHSlider2.connect("value_changed", AudioManager.set_sfx_volume)
