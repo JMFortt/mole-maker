71d814f147fed46342a4ae543c5688707aab1e63
Julia Fortt <juliamfortt@gmail.com>
2025-05-08 12:39:38 -0400

    change atom assembler controls to remove text component

--- scripts/add_atom_component_button.gd
+++ scripts/add_atom_component_button.gd
@@ -4,15 +4,28 @@ extends Button
 
 func _on_add_button_pressed() -> void:
 	AudioManager.button_click()
-	var line_edit = get_parent().get_node("LineEdit")
 	if get_parent().name == "Protons":
 		#print("adding protons")
-		root.add_atom_components("proton", int(line_edit.text))
+		root.add_atom_components("proton", 1)
 	elif get_parent().name == "Neutrons":
-		root.add_atom_components("neutron", int(line_edit.text))
+		root.add_atom_components("neutron", 1)
 		#print("adding neutrons")
 	elif get_parent().name == "Electrons":
-		root.add_atom_components("electron", int(line_edit.text))
+		root.add_atom_components("electron", 1)
+		#print("adding electrons")
+	else:
+		push_error("Invalid 'add button' parent!")
+
+func _on_subtract_button_pressed() -> void:
+	AudioManager.button_click()
+	if get_parent().name == "Protons":
+		#print("adding protons")
+		root.add_atom_components("proton", -1)
+	elif get_parent().name == "Neutrons":
+		root.add_atom_components("neutron", -1)
+		#print("adding neutrons")
+	elif get_parent().name == "Electrons":
+		root.add_atom_components("electron", -1)
 		#print("adding electrons")
 	else:
 		push_error("Invalid 'add button' parent!")
